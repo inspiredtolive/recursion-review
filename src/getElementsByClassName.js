9d26d8e823e03a1e2b7176970a9c384de00d5cee
inspiredtolive <inspiredtolive0@gmail.com>
2016-07-25 15:16:57 -0700

    passes all tests in SpecRunner

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,6 +4,21 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className) {
-  // your code here
+var getElementsByClassName = function(className, element) {
+  var result = [];
+  if (element === undefined) {
+    element = document.body;
+  }
+  if (element.classList) {
+    if (element.className.split(' ').indexOf(className) > -1) {
+      result.push(element);
+    }
+  }
+  if (element.childNodes) {
+    element.childNodes.forEach(function(node) {
+      result = result.concat( getElementsByClassName(className, node) );
+    });
+  }
+  return result;
 };
+
