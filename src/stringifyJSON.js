16562310c5f79d25d25b9a13a1b6fe995ef62d1b
inspiredtolive <inspiredtolive0@gmail.com>
2016-07-25 15:18:29 -0700

    passes all tests in SpecRunner

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,35 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  if (Object.prototype.toString.call(obj) === '[object Function]') {
+    return null;
+  }
+
+  if (obj === undefined) {
+    return null;
+  }
+
+  if ( Array.isArray(obj) ) {
+    return '[' +
+    obj.map(function(item) {
+      return stringifyJSON(item);
+    }).join(',') + ']';
+  }
+
+  if (Object.prototype.toString.call(obj) === '[object Object]') {
+    var val;
+    var result = [];
+    Object.keys(obj).forEach(function(key) {
+      val = stringifyJSON(obj[key]);
+      if (val !== null) {
+        result.push('\"' + key + '\":' + val);
+      }
+    });
+    return '{' + result.join(',') + '}';
+  }
+  return '' + obj;
 };
